f5edc2d7d71eda94ba3eeb6117dfc486ad27d9ad
Sanad Thapa <sanadtm@gmail.com>
2025-03-11 20:14:24 -0700

    ST -  Beautified the Table

--- test.js
+++ test.js
@@ -8,8 +8,21 @@ function addTo(a1){
 console.log(addTo(2)(3))
 
 
-fetch("https://restcountries.com/v3.1/name/nepal").then(data=>{
-    return data.json();
-}).then(data=>{
-    console.log(data)
-})
+// fetch("https://restcountries.com/v3.1/name/nepal").then(data=>{
+//     return data.json();
+// }).then(data=>{
+//     console.log(data)
+// })
+
+async function fetchCountry() {
+    try {
+        let response = await fetch("https://restcountries.com/v3.1/name/nepal");
+        let data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+// Call the function
+fetchCountry();
